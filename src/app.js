7005d5283493d6c80cd5b078971dbc290faddb21
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-28 15:49:42 +0100

    created home and index routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
+import 'bulma'
+
 import Navbar from './components/common/Navbar'
 import EventsIndex from './components/events/EventsIndex'
-import 'bulma'
+import Home from './components/common/Home'
 
 
 class App extends React.Component {
   render(){
     return(
-      <div>
-        <Navbar />
-        <EventsIndex />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path='/events' component={EventsIndex} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
