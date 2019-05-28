27ab0320f9fd8b1c2e3c0ecc16120cb6c0fc8bf6
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-28 16:33:08 +0100

    added link to events index in navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,41 +1,46 @@
 import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
-  return(
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <figure className="image is-48x48">
-          <img src="http://www.clker.com/cliparts/8/5/7/2/13448089181230741562black-circle-md[1]-hi.png" />
-        </figure>
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+class Navbar extends React.Component {
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">
-            Browse
+  render(){
+    return(
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <figure className="image is-48x48">
+            <img src="http://www.clker.com/cliparts/8/5/7/2/13448089181230741562black-circle-md[1]-hi.png" />
+          </figure>
+          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
         </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Register</strong>
-              </a>
-              <a className="button is-light">
-                Log in
-              </a>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/events" className="navbar-item">
+              Browse exhibitions
+            </Link>
+          </div>
+
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Register</strong>
+                </a>
+                <a className="button is-light">
+                  Log in
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
-export default Navbar
+export default withRouter(Navbar)
