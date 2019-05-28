4f4e2ae7b67c70c6e6899c680be45c5f69cacd33
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-28 16:04:55 +0100

    added what's on section

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import SearchBar from 'react-native-elements'
+import FeaturedEvents from '../../components/events/FeaturedEvents.js'
+
 
 class Home extends React.Component {
   render(){
     return(
       <div>
-        home
+        <section className="hero is-info is-large">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Title Home
+              </h1>
+            </div>
+          </div>
+        </section>
+        <FeaturedEvents />
+        <SearchBar />
       </div>
     )
   }
