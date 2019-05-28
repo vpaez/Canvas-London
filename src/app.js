f8e3d8e4187b4bc5e061f70d3a06f77ce67fe90a
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-28 15:09:37 +0100

    created home component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import Home from './components/Home'
 
 class App extends React.Component {
   render(){
     return(
-      <div>hi</div>
+      <Home />
     )
   }
 }
