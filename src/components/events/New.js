e59a83abd4c827a4bb65692c3aecc181866fc323
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-29 12:01:01 +0100

    set up form and new files

---
+++ src/components/events/New.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios'
+
+import Form from './Form'
+import Auth from '../../lib/Auth'
+
+class New extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      data: {},
+      errors: {},
+      vinyls: []
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    this.setState({ data })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    const token = Auth.getToken()
+
+    axios.post('/api/vinyls', this.state.data, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    })
+      .then(() => this.props.history.push('/vinyls'))
+      .catch(err => this.setState({ errors: err.response.data.errors }))
+  }
+
+  render() {
+
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-half-desktop is-two-thirds-tablet">
+              <Form
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                data={this.state.data}
+                errors={this.state.errors}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default New
