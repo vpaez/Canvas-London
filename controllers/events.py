1479aee7e4a5d302c3d3058b64f377d3faef72b5
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-29 13:26:49 +0100

    added edit controller with user not authorised error message

--- controllers/events.py
+++ controllers/events.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify, g, abort
 from pony.orm import db_session
-from flask import Blueprint, request, jsonify, g
 from marshmallow import ValidationError
 from app import db
 from lib.secure_route import secure_route
@@ -42,4 +41,24 @@ def show(event_id):
 
     if not event:
         abort(404)
+    return schema.dumps(event)
+
+@router.route('/events/<int:event_id>', methods=['PUT'])
+@db_session
+@secure_route
+
+def update(event_id):
+    schema = EventSchema()
+    event = Event.get(id=event_id)
+    if not event:
+        return jsonify({'message': 'Event not found'}), 404
+    if not event.user.id == g.current_user.id:
+        return jsonify({'message': 'User not authorized'}), 404
+    try:
+        data = schema.load(request.get_json())
+        event.set(**data)
+        db.commit()
+    except ValidationError as err:
+        return jsonify({'message': 'Validation failed', 'errors': err.messages}), 422
+
     return schema.dumps(event)
