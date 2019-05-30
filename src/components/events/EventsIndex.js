1fca7f406d26760967b0293f07ef941164fdd2ed
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-30 14:51:44 +0100

    got upcoming filter working

--- src/components/events/EventsIndex.js
+++ src/components/events/EventsIndex.js
@@ -24,6 +24,34 @@ function whatsOn(arr) {
   })
 }
 
+function comingSoon(arr){
+  return arr.filter(exhib => {
+
+    const startSplitDate = exhib.start_date.split('/')
+    const startMonth = startSplitDate[1] - 1
+    const startDate = new Date(startSplitDate[2], startMonth, startSplitDate[0])
+
+    const startParsed = Date.parse(startDate)
+    const currentParsed = Date.parse(new Date())
+
+    if (currentParsed < startParsed ) return exhib
+  })
+}
+
+function past(){
+  return arr.filter(exhib => {
+    const endSplitDate = exhib.end_date.split('/')
+    const endMonth = endSplitDate[1] - 1
+    const endDate = new Date(endSplitDate[2], endMonth, endSplitDate[0])
+
+    const endParsed = Date.parse(endDate)
+    const currentParsed = Date.parse(new Date())
+
+
+
+  })
+}
+
 function orderByDate(arr) {
   return arr.slice().sort(function (a, b) {
 
@@ -66,15 +94,16 @@ class EventsIndex extends React.Component {
   }
 
   handleChange(e){
-    console.log(e)
     axios.get('/api/events')
       .then(res => {
         if (e.label === 'Current'){
           const current = whatsOn(res.data)
           const currentSorted = orderByDate(current)
-          console.log(this)
           this.setState({exhibitions: currentSorted})
-        }
+        } else if (e.label === 'Upcoming') {
+          const upcoming = comingSoon(res.data)
+          this.setState({exhibitions: upcoming})
+        } 
       })
   }
 
