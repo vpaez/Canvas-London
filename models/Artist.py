7975ca379e93428b94327e5d12880bf65170a8a4
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-31 09:49:09 +0100

    updated artist model

--- models/Artist.py
+++ models/Artist.py
@@ -1,12 +1,21 @@
 from app import db
-from pony.orm import Required
+from pony.orm import Required, Optional, Set
 from marshmallow import Schema, fields
 
 
 class Artist(db.Entity):
     name = Required(str)
+    description = Optional(str)
+    dob = Optional(str)
+    dod = Optional(str)
+    events = Set('Event')
 
 
 class ArtistSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+    description = fields.Str()
+    dob = fields.Str()
+    dob = fields.Str()
+    dod = fields.Str()
+    events = fields.Nested('UserSchema', many=True, exclude=('keywords', 'artists', 'user'))
