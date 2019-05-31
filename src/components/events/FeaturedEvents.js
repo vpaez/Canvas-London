76fa8b8ea0da57c2f3ae579c93bbdbf07a47631b
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-31 11:19:53 +0100

    added link from what's on to show page

--- src/components/events/FeaturedEvents.js
+++ src/components/events/FeaturedEvents.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const FeaturedEvents = ({exhibitions}) => {
   return(
@@ -8,13 +9,16 @@ const FeaturedEvents = ({exhibitions}) => {
         <div className="columns is-multiline">
           {exhibitions.map(exhibition =>
             <div key={exhibition.id} className="column is-one-quarter-desktop is-one-third-tablet">
-              <figure>
-                <img src={exhibition.image} alt={exhibition.name}/>
-              </figure>
-              <h2 className="title is-4">{exhibition.name}</h2>
-              <p>Date: {`${exhibition.start_date} - ${exhibition.end_date}`}</p>
-              <p>{exhibition.venue}</p>
+              <Link to={`/events/${exhibition.id}`}>
+                <figure>
+                  <img src={exhibition.image} alt={exhibition.name}/>
+                </figure>
+                <h2 className="title is-4">{exhibition.name}</h2>
+                <p>Date: {`${exhibition.start_date} - ${exhibition.end_date}`}</p>
+                <p>{exhibition.venue}</p>
+              </Link>
             </div>
+
           )}
         </div>
       </div>
