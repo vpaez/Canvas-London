3dee8f72d40322140cdc414f3fb0728bfc56bb45
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-06-03 16:28:24 +0100

    Refactored near me using before insert

--- src/components/events/NearMe.js
+++ src/components/events/NearMe.js
@@ -2,19 +2,19 @@ import React from 'react'
 import axios from 'axios'
 
 
-    Math.radians = function(degrees) {
-      return degrees * Math.PI / 180
-    }
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180
+}
 
 
-    function calculateDistance(lat,lon,userLat,userLon){
+function calculateDistance(lat,lon,userLat,userLon){
 
-      var distance = ( 6371 * Math.acos( Math.cos( Math.radians(lat) ) * Math.cos( Math.radians( userLat ) )
-                        * Math.cos( Math.radians( userLon) - Math.radians(lon) ) + Math.sin( Math.radians(lat) ) * Math.sin(Math.radians(userLat)) ) )*1000
-      console.log(distance+'  meter')
-      console.log(distance)
-      return distance
-    }
+  var distance = ( 6371 * Math.acos( Math.cos( Math.radians(lat) ) * Math.cos( Math.radians( userLat ) )
+                    * Math.cos( Math.radians( userLon) - Math.radians(lon) ) + Math.sin( Math.radians(lat) ) * Math.sin(Math.radians(userLat)) ) )*1000
+  console.log(distance+'  meter')
+  console.log(distance)
+  return distance
+}
 
 
 class NearMe extends React.Component {
@@ -23,40 +23,21 @@ class NearMe extends React.Component {
     this.state = {}
   }
   componentDidMount(){
-    navigator.geolocation.watchPosition((position) => {
-      const { latitude, longitude } = position.coords
-      this.setState({ location: { lat: latitude, lon: longitude } })
-    })
     axios.get('/api/events')
-      .then(res => {
+      .then(res => this.setState({exhibitions: res.data }))
+      .then(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords
+          console.log(this.state)
+          const nearby = this.state.exhibitions.filter(exhibition =>{
+            console.log(longitude, exhibition.venue.lng)
+            return (calculateDistance(longitude, latitude, exhibition.lng, exhibition.lat, 3 ) < 2000)
 
-        this.setState({exhibitions: res.data})
-        const empty = []
+          })
 
-        this.state.exhibitions.filter(exhibition =>{
-
-          axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${exhibition.venue}&key=7be1f0737d694364bb498795cd346bf3`)
-            .then(res => {
-              const venueCoordinates = res.data.results[0].geometry
-              console.log(venueCoordinates)
-              console.log(exhibition)
-
-              if (calculateDistance(this.state.location.lon,this.state.location.lat, venueCoordinates.lng, venueCoordinates.lat, 3 ) < 2000){
-
-                console.log('done')
-
-                console.log(exhibition)
-
-                console.log(this.state)
-                empty.push(exhibition)
-
-              }
-            })
-
+          this.setState({ nearby })
         })
-        this.setState( {nearby: empty}  )
       })
-
 
 
   }
