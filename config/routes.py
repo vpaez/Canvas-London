c6d393a14d0058871c7652dbaa136c841a873783
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-06-05 07:19:28 +0100

    created heroku app

--- config/routes.py
+++ config/routes.py
@@ -8,3 +8,12 @@ app.register_blueprint(events.router, url_prefix='/api')
 app.register_blueprint(keywords.router, url_prefix='/api')
 app.register_blueprint(artists.router, url_prefix='/api')
 app.register_blueprint(contacts.router, url_prefix='/api')
+
+@app.route('/')
+@app.route('/<path:path>')
+def catch_all(path='index.html'):
+
+    if os.path.isfile('public/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
