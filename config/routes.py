afb2a1cb7a3db043da6b9ea20888a1c1509e94db
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-06-05 07:19:50 +0100

    updated routes file

--- config/routes.py
+++ config/routes.py
@@ -8,3 +8,12 @@ app.register_blueprint(events.router, url_prefix='/api')
 app.register_blueprint(keywords.router, url_prefix='/api')
 app.register_blueprint(artists.router, url_prefix='/api')
 app.register_blueprint(contacts.router, url_prefix='/api')
+
+@app.route('/') # homepage
+@app.route('/<path:path>') # any other path
+def catch_all(path='index.html'):
+
+    if os.path.isfile('public/' + path): # if path is a file, send it back
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
